74194bfb57bcde72d43ebd1435f29e3617d2c991
Bekkalizer <markus.bekken@gmail.com>
2016-06-20 20:50:17 +0200

    Syntax error fix

--- webpack.config.js
+++ webpack.config.js
@@ -57,13 +57,13 @@ module.exports = {
         inline: true,
         compress: true,
         proxy: [
-            { path: '/api/*', target: dhisConfig.baseUrl, bypass },
-            { path: '/dhis-web-commons-ajax-json/*', target: dhisConfig.baseUrl, bypass },
-            { path: '/dhis-web-commons-stream/*', target: dhisConfig.baseUrl, bypass },
-            { path: '/dhis-web-commons/*', target: dhisConfig.baseUrl, bypass },
-            { path: '/icons/*', target: dhisConfig.baseUrl, bypass },
-            { path: '/images/*', target: dhisConfig.baseUrl, bypass },
-            { path: '/main.js', target: dhisConfig.baseUrl, bypass }
+            { path: '/api/*', target: dhisConfig.baseUrl, bypass: bypass },
+            { path: '/dhis-web-commons-ajax-json/*', target: dhisConfig.baseUrl, bypass: bypass },
+            { path: '/dhis-web-commons-stream/*', target: dhisConfig.baseUrl, bypass: bypass },
+            { path: '/dhis-web-commons/*', target: dhisConfig.baseUrl, bypass: bypass },
+            { path: '/icons/*', target: dhisConfig.baseUrl, bypass: bypass },
+            { path: '/images/*', target: dhisConfig.baseUrl, bypass: bypass },
+            { path: '/main.js', target: dhisConfig.baseUrl, bypass: bypass }
         ]
     },
 };
