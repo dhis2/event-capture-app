5d53fd9544abccb49e146f95d00b72d59c461df4
zepto5 <andre.hukkelaas@abaris.no>
2018-01-11 13:57:21 +0100

    Temporary ALL authority fix

--- core/event-capture.js
+++ core/event-capture.js
@@ -289,13 +289,24 @@ function getOrgUnitLevels()
 function getProgramAccess(){
     return dhis2.tracker.getTrackerObjects('programAccess','programs', DHIS2URL+'/programs.json', 'filter=programType:eq:WITHOUT_REGISTRATION&paging=false&fields=id,access[data[read,write]],programStages[access[data[read,write]]]','temp', dhis2.ec.store).then(function(programAccesses){
         //Temporary getting programStageAccess
+        var SessionStorageService = angular.element('body').injector().get('SessionStorageService');    
+        var userProfile = SessionStorageService.get('USER_PROFILE');
+        var hasAllAccess = false;
+        if(userProfile && userProfile.authorities){
+            var r = $.grep(userProfile.authorities, function(a){ return a === 'ALL'});
+            if(r.length > 0) hasAllAccess = true;
+        }
+
         var programAccessesById = {};
         _.each(_.values(programAccesses), function(programAccess){
+            if(hasAllAccess) programAccess.access.data = {read: true, write: true };
             programAccess.programStages = [];
             programAccessesById[programAccess.id] = programAccess;
         });
+
         return dhis2.tracker.getTrackerObjects('programStageAccess','programStages', DHIS2URL+'/programStages.json', 'paging=false&fields=id,program,access[data[read,write]]','temp', dhis2.ec.store).then(function(programStageAccesses){
             _.each(_.values(programStageAccesses), function(programStageAccess){
+                if(hasAllAccess) programStageAccess.access.data = {read : true, write: true};
                 if(programAccessesById[programStageAccess.program.id]){
                     programAccessesById[programStageAccess.program.id].programStages.push(programStageAccess);
                 }
