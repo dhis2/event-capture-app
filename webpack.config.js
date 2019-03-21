828040030dedb5bebc295ffe4bb65124d30cf07d
Joakim <joakim.melseth@abaris.no>
2019-03-21 15:53:20 +0100

    load category options separately

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,6 @@ module.exports = {
                 ['styles/style.css'],
             ]),
             scripts: makeScriptTags([
-                'core/event-capture.js',
                 '../main.js',
             ]),
         })
